f6db6e872b60efacfdc533d625a1c3a71aa8109c
Jack Platten <jack@platten.me>
2022-03-30 17:20:18 -0700

    Add completions / remove `op` from `config.fish`

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -4,7 +4,7 @@ end
 
 set fish_greeting
 
-op completion fish | source
+#op completion fish | source
 starship init fish | source
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
