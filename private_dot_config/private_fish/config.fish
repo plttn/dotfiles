6f89de40be04cd3f05b9fb059061705392ca2c18
Jack Platten <jack@platten.me>
2024-01-15 12:45:26 -0800

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -3,7 +3,7 @@ set --export fish_color_autosuggestion 555
 if status is-interactive
     # Commands to run in interactive sessions can go here
     atuin init fish | source
-
+     export LS_COLORS="$(vivid generate molokai)"
 end
 
 
