dcbfa6d0d475d5dd7e0920ef58f88744a16a8a0e
Jack Platten <jack@platten.me>
2024-02-12 14:19:14 -0800

    Update .wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -15,7 +15,7 @@ end
 -- For example, changing the color scheme:
 config.color_scheme = 'Brogrammer'
 
-config.font = wezterm.font('Berkeley Mono')
+config.font = wezterm.font('CommitMono')
 config.harfbuzz_features = { 'calt=0' } --disable ligatures for Berkeley Mono
 config.font_size = 13
 config.hide_tab_bar_if_only_one_tab = true
