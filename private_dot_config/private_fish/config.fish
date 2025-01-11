af281be9205dfb06a3e89891e80dddb804b0b20a
Jack Platten <jack@platten.me>
2025-01-10 20:25:09 -0800

    Update .config/ghostty/config Update .config/fish/config.fish Update .config/fish/fish_plugins

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,10 +1,11 @@
 # set --export fish_color_autosuggestion 555
 #set -g tide_right_prompt_suffix " "
 
+set -g EZA_CONFIG_DIR "$HOME/.config/eza"
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     atuin init fish | source
-    export LS_COLORS="$(vivid generate molokai)"
 end
 
 
@@ -20,12 +21,10 @@ end
 
 # source ~/.asdf/asdf.fish
 # direnv hook fish | source
-
-abbr --position command kssh "kitten ssh"
-
 #starship init fish | source
 
 # tabtab source for packages
 # uninstall by removing these lines
 [ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
 /Users/jack/.local/bin/mise activate fish | source
+scheme set monokai
