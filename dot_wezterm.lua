45e8261b1ad51255708f6b9c27ebee69a1bf7692
Jack Platten <jack@platten.me>
2025-03-04 15:16:56 -0800

    Update .wezterm.lua Update .config/ghostty/config Update .config/git/config Update .config/zed/settings.json

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -17,8 +17,9 @@ config.color_scheme = 'Brogrammer'
 
 
 config.font = wezterm.font('TX-02')
-onfig.harfbuzz_features = { 'calt=0' }
-config.font_size = 12
+
+config.harfbuzz_features = { 'calt=0' }
+onfig.font_size = 12
 config.hide_tab_bar_if_only_one_tab = true
 config.initial_cols = 115
 config.initial_rows = 35
