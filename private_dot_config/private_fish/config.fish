57309f9861591b0be142f691c196ab6d0431f4e1
Jack Platten <jack@platten.me>
2025-09-29 22:59:32 -0700

    Update .config/atuin/config.toml Update .config/ghostty/config Update .config/mise/config.toml Update .config/fish/config.fish Update .config/fish/fish_plugins Update .config/zed/settings.json

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,13 +2,13 @@ set -g EZA_CONFIG_DIR "$HOME/.config/eza"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    atuin init fish | source
+    atuin init fish --disable-up-arrow | source
 
     export LS_COLORS="$(vivid generate molokai)"
 
 end
 
-zoxide init fish | source
+#zoxide init fish | source
 # pnpm
 # set -gx PNPM_HOME "/Users/jack/Library/pnpm"
 # if not string match -q -- $PNPM_HOME $PATH
@@ -16,10 +16,10 @@ zoxide init fish | source
 # end
 # pnpm end
 
-
 # tabtab source for packages
 # uninstall by removing these lines
 [ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
 
-scheme set monokai
+#scheme set monokai
 ~/.local/bin/mise activate fish | source
+bind up _atuin_bind_up
